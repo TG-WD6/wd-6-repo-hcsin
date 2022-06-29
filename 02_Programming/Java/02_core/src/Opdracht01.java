8e05158e19518d7c4e81ffa35629a50903dc8cf9
Hiu-Chee  Sin <aggutino@hotmail.com>
2022-06-29 09:57:15 +0200

    02 core opdrachten

---
+++ 02_Programming/Java/02_core/src/Opdracht01.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Opdracht01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write("Hello world");
+            writer.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
