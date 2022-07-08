b86082417c02adc9685176d8e09ee85095ec8624
Hiu-Chee  Sin <aggutino@hotmail.com>
2022-07-08 16:21:11 +0200

    retro week 8

--- 02_Programming/Java/02_core/airport/src/Airport.java
+++ 02_Programming/Java/02_core/airport/src/Airport.java
@@ -1,5 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Airport {
     public static void main(String[] args) {
-
+//        try {
+//            BufferedWriter bw = new BufferedWriter(new FileWriter("./resources/test.txt"));
+//            bw.write("today");
+//            bw.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        String path = "./resources/countries.csv";
+        String line = "";
+//        File file = new File(path);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            while ((line = br.readLine()) != null){
+//                System.out.println(line);
+                String[] values = line.split(",");
+                System.out.println(values[0]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        Scanner scan = new Scanner(file);
+//        System.out.println(scan.nextLine());
     }
 }
---
+++ Retro/week8.md
@@ -0,0 +1,15 @@
+# Retro Week \##
+
+## Wat ging goed
+
+1. zoals de plannen alles opdrachten afgemaakt
+2. krijg iets meer skills met parameters doorsturen tussen verschillende classes
+
+## Wat ging er minder goed
+
+1. beetje stress gehad toen ik dat er weinig tijd over heb voor de eindopdracht
+2. 
+
+## Actiepunten voor volgende week
+
+- voor de volgende handout, kan ik beter alles eerst doornemen ipv een voor een nemen, zodat ik meer zicht krijg wat er op komt.
